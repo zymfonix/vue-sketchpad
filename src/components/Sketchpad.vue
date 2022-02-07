2d17ec98e41234c424fbd34c407f9d3a7870272c
Tor Morten Jensen <tormorten@tormorten.no>
2022-02-07 13:37:31 +0100

    init

---
+++ src/components/Sketchpad.vue
@@ -0,0 +1,126 @@
+<template>
+  <svg
+      style="width: 100%; height: 100%; display: block; touch-action: none"
+      ref="canvas"
+      @mousedown="handlePointerDown"
+      @mousemove="handlePointerMove"
+      @mouseup="handlePointerUp"
+  >
+    <path :d="pathData" :fill="color" />
+    <path v-for="(path, index) in pathsWithData" :key="`path${index}`" :d="path.path" :fill="path.color" />
+  </svg>
+</template>
+
+<script>
+import {getStroke} from 'perfect-freehand'
+import Canvg from 'canvg';
+import {getSvgPathFromStroke} from '../utils'
+
+export default {
+  props: {
+    color: {
+      type: String,
+      default() {
+        return '#000'
+      }
+    },
+    options: {
+      type: Object,
+      default() {
+        return {};
+      }
+    }
+  },
+  data() {
+    return {
+      points: [],
+      paths: [],
+
+    }
+  },
+
+  computed: {
+    allOptions() {
+      return {
+        size: 4,
+        thinning: 0.5,
+        smoothing: 0.5,
+        streamline: 0.5,
+        easing: (t) => t,
+        start: {
+          taper: 0,
+          easing: (t) => t,
+          cap: true
+        },
+        end: {
+          taper: 100,
+          easing: (t) => t,
+          cap: true
+        },
+        ...this.options
+      }
+    },
+    stroke() {
+      return getStroke(this.points, this.allOptions);
+    },
+
+    pathData() {
+      return getSvgPathFromStroke(this.stroke);
+    },
+
+    pathsWithData() {
+      return JSON.parse(JSON.stringify(this.paths)).map(points => {
+        points.path = getSvgPathFromStroke(getStroke(points.path, this.allOptions));
+        return points;
+      })
+    }
+  },
+
+  methods: {
+    getCanvasX(e) {
+      if (!this.$refs.canvas) return e.pageX;
+      return e.pageX - this.$refs.canvas.getBoundingClientRect().left - window.scrollX;
+    },
+
+    getCanvasY(e) {
+      if (!this.$refs.canvas) return e.pageY;
+      return e.pageY - this.$refs.canvas.getBoundingClientRect().top - window.scrollY;
+    },
+
+    handlePointerDown(e) {
+      this.points = [[this.getCanvasX(e), this.getCanvasY(e), e.pressure]]
+    },
+
+    handlePointerMove(e) {
+      if (e.buttons !== 1) return;
+      this.points = [...this.points, [this.getCanvasX(e), this.getCanvasY(e), e.pressure]]
+    },
+
+    handlePointerUp() {
+      let path = JSON.parse(JSON.stringify(this.points))
+      this.paths.push({
+        path,
+        color: this.color.toString()
+      });
+      this.points = []
+    },
+
+    clear() {
+      this.paths = [];
+    },
+
+    async export() {
+      const canvas = document.createElement('canvas');
+      const ctx = canvas.getContext('2d');
+      canvas.width = this.$refs.canvas.clientWidth;
+      canvas.height = this.$refs.canvas.clientHeight;
+      let item = await Canvg.fromString(ctx, this.$refs.canvas.outerHTML);
+
+      item.start();
+
+      return canvas.toDataURL();
+    },
+
+  }
+};
+</script>
