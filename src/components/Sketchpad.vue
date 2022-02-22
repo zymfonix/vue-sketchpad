73d0e6bbd97dd14ba631ad440cae6d0b8a77cc51
Tor Morten Jensen <tormorten@tormorten.no>
2022-02-22 13:27:16 +0100

    working touch support

--- src/components/Sketchpad.vue
+++ src/components/Sketchpad.vue
@@ -1,20 +1,30 @@
 <template>
-  <svg
-      style="width: 100%; height: 100%; display: block; touch-action: none"
-      ref="canvas"
-      @mousedown="handlePointerDown"
-      @mousemove="handlePointerMove"
-      @mouseup="handlePointerUp"
+  <svg style="touch-action: none"
+       ref="canvas"
+       class="fit block"
+       @mousedown="handlePointerDown"
+       @mousemove="handlePointerMove"
+       @mouseup="handlePointerUp"
+       @mouseleave="handlePointerUp"
+       @touchstart="handleTouchDown"
+       @touchmove="handleTouchMove"
+       @touchend="handleTouchUp"
+       @touchcancel="handleTouchUp"
   >
-    <path v-for="(path, index) in pathsWithData" :key="`path${index}`" :d="path.path" :fill="path.color" />
-    <path :d="pathData" :fill="color" />
+    <path v-for="(path, index) in pathsWithData"
+          :key="`path${index}`"
+          :d="path.path"
+          :fill="path.color" />
+
+    <path :d="pathData"
+          :fill="color" />
   </svg>
 </template>
 
 <script>
-import {getStroke} from 'perfect-freehand'
+import {getStroke} from 'perfect-freehand';
 import Canvg from 'canvg';
-import {getSvgPathFromStroke} from '../utils'
+import {getSvgPathFromStroke} from 'vue-zketchpad/src/utils';
 
 export default {
   emits: ['update:modelValue'],
@@ -23,28 +33,28 @@ export default {
     color: {
       type: String,
       default() {
-        return '#000'
-      }
+        return '#000';
+      },
     },
     options: {
       type: Object,
       default() {
         return {};
-      }
+      },
     },
     modelValue: {
       type: Array,
       default() {
-        return []
-      }
-    }
+        return [];
+      },
+    },
   },
 
   data() {
     return {
       points: [],
-      paths: [],
-    }
+      paths: this.modelValue,
+    };
   },
 
   computed: {
@@ -54,20 +64,21 @@ export default {
         thinning: 0.5,
         smoothing: 0.5,
         streamline: 0.5,
-        easing: (t) => t,
+        easing: t => t,
         start: {
           taper: 0,
-          easing: (t) => t,
-          cap: true
+          easing: t => t,
+          cap: true,
         },
         end: {
           taper: 100,
-          easing: (t) => t,
-          cap: true
+          easing: t => t,
+          cap: true,
         },
-        ...this.options
-      }
+        ...this.options,
+      };
     },
+
     stroke() {
       return getStroke(this.points, this.allOptions);
     },
@@ -78,39 +89,83 @@ export default {
 
     pathsWithData() {
       return JSON.parse(JSON.stringify(this.paths)).map(points => {
-        points.path = getSvgPathFromStroke(getStroke(points.path, this.allOptions));
+        points.path = getSvgPathFromStroke(
+            getStroke(points.path, this.allOptions),
+        );
+
         return points;
-      })
-    }
+      });
+    },
   },
 
   methods: {
-    getCanvasX(e) {
-      if (!this.$refs.canvas) return e.pageX;
-      return e.pageX - this.$refs.canvas.getBoundingClientRect().left - window.scrollX;
+    getCanvasX(e, type = 'mouse') {
+      let moveX = e.pageX;
+      if (type === 'touch') {
+        moveX = e.touches[0].clientX;
+      }
+
+      if (!this.$refs.canvas) {
+        return moveX;
+      }
+
+      return moveX - this.$refs.canvas.getBoundingClientRect().left - window.scrollX;
     },
 
-    getCanvasY(e) {
-      if (!this.$refs.canvas) return e.pageY;
-      return e.pageY - this.$refs.canvas.getBoundingClientRect().top - window.scrollY;
+    getCanvasY(e, type = 'mouse') {
+      let moveY = e.pageY;
+      if (type === 'touch') {
+        moveY = e.touches[0].clientY;
+      }
+
+      return moveY - this.$refs.canvas.getBoundingClientRect().top - window.scrollY;
     },
 
     handlePointerDown(e) {
-      this.points = [[this.getCanvasX(e), this.getCanvasY(e), e.pressure]]
+      this.points = [
+        [this.getCanvasX(e), this.getCanvasY(e), e.pressure],
+      ];
     },
 
     handlePointerMove(e) {
       if (e.buttons !== 1) return;
-      this.points = [...this.points, [this.getCanvasX(e), this.getCanvasY(e), e.pressure]]
+      this.points = [
+        ...this.points,
+        [this.getCanvasX(e), this.getCanvasY(e), e.pressure],
+      ];
     },
 
     handlePointerUp() {
-      let path = JSON.parse(JSON.stringify(this.points))
+      const path = JSON.parse(JSON.stringify(this.points));
+
       this.paths.push({
         path,
-        color: this.color.toString()
+        color: this.color.toString(),
       });
-      this.points = []
+      this.points = [];
+    },
+
+    handleTouchDown(e) {
+      this.points = [
+        [this.getCanvasX(e, 'touch'), this.getCanvasY(e, 'touch'), e.pressure],
+      ];
+    },
+
+    handleTouchMove(e) {
+      this.points = [
+        ...this.points,
+        [this.getCanvasX(e, 'touch'), this.getCanvasY(e, 'touch'), e.pressure],
+      ];
+    },
+
+    handleTouchUp() {
+      const path = JSON.parse(JSON.stringify(this.points));
+
+      this.paths.push({
+        path,
+        color: this.color.toString(),
+      });
+      this.points = [];
     },
 
     clear() {
@@ -120,31 +175,39 @@ export default {
     async export() {
       const canvas = document.createElement('canvas');
       const ctx = canvas.getContext('2d');
+
       canvas.width = this.$refs.canvas.clientWidth;
       canvas.height = this.$refs.canvas.clientHeight;
-      let item = await Canvg.fromString(ctx, this.$refs.canvas.outerHTML);
+
+      const item = await Canvg.fromString(ctx, this.$refs.canvas.outerHTML);
 
       item.start();
-
       return canvas.toDataURL();
     },
 
+    wasUpdated(newVal, oldVal) {
+      return JSON.stringify(newVal) !== JSON.stringify(oldVal);
+    },
   },
 
   watch: {
     paths: {
       deep: true,
       handler(value) {
-        if (value !== this.modelValue) this.$emit('update:modelValue', value)
-      }
+        if (this.wasUpdated(value, this.modelValue)) {
+          this.$emit('update:modelValue', value);
+        }
+      },
     },
 
     modelValue: {
       deep: true,
       handler(value) {
-        if (value !== this.paths) this.paths = value;
-      }
-    }
-  }
+        if (this.wasUpdated(value, this.paths)) {
+          this.paths = value;
+        }
+      },
+    },
+  },
 };
 </script>
